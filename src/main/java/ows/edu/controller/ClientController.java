7adc1f009569651c86c1c86630cd00887bceb1a0
kim-yewon <kim-yewon@192.168.2.44>
2022-06-21 14:50:39 +0900

    client

---
+++ src/main/java/ows/edu/controller/ClientController.java
@@ -0,0 +1,47 @@
+package ows.edu.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.log4j.Log4j2;
+import ows.edu.dto.Client;
+import ows.edu.service.ClientService;
+
+@RestController
+@RequestMapping("/client")
+@Log4j2
+public class ClientController {
+	
+	@Resource
+	ClientService clientService;
+	
+	//거래처 목록 불러오기
+	@GetMapping("/")
+	public List<Client> getClientNameList() {
+		List<Client> list = new ArrayList<>();
+		list = clientService.getlist();
+//		log.info("clientName : " + clientName);
+		log.info("clientcontroller : " + list);
+		return list;
+	}
+	
+	
+//	@GetMapping("/main")
+//	public Map<String, Object> getClientNameList(@RequestParam(value="clientName") String clientName) {
+//		List<String> clientNameList = new ArrayList<>();
+//		clientNameList = clientService.getlist(clientName);
+//		log.info("clientName : " + clientName);
+//		log.info("clientcontroller : " + clientNameList);
+//		
+//		Map<String, Object> map = new HashMap<>();
+//		map.put("clientName", clientName);
+//		return map;
+//	}
+	
+}
