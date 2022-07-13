33cd835beae3ae532cd7ba188b3d457a1fca6872
yewon6282 <yewon6282@gmail.com>
2022-07-13 14:59:58 +0900

    .

--- src/main/java/ows/edu/dto/OrderItem.java
+++ src/main/java/ows/edu/dto/OrderItem.java
@@ -8,11 +8,6 @@ public class OrderItem {
 	private Long orderNo;
 	private String itemCode;
 	private int qty;
-<<<<<<< HEAD
 	private int unreleaseQuantity;
-=======
-	private int orderItemUnreleaseQuantity;
 	private String orderItemNote;
-		
->>>>>>> branch 'master' of http://kosa3.iptime.org:13000/4ever/final-back-end.git
 }
