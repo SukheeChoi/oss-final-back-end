6f346bcdd882ac8348d30167ba9515c6bc6acca8
yewon6282 <yewon6282@gmail.com>
2022-07-21 17:05:05 +0900

    미출고, 출고 분

--- src/main/java/ows/edu/service/ClientService.java
+++ src/main/java/ows/edu/service/ClientService.java
@@ -25,33 +25,12 @@ public class ClientService {
 	public List<Client> getList(Map<String, Object> map){
 		return clientDao.getList(map);
 	}
-//	public List<HashMap<String, String>> getList(
-//			String[] shippingCategory,
-//			int status,
-//			boolean unrelease,
-//			Long orderNo,
-//			String clientName,
-//			Pager pager,
-//			int pageSize
-//			) {
-//		return clientDao.getList(shippingCategory, status, unrelease, orderNo, clientName, pager, pageSize);
-//	};
 	
 	//주문 단계를 전체 선택했을 때, 배송구분과 미출고로 필터링
 	public List<Client> getListByShippingCategory(Map<String, Object> map){
 		log.info("ClientService - map " + map.keySet());
 		return clientDao.getListByShippingCategory(map);
 	}
-//	public List<HashMap<String, String>> getListByShippingCategory(
-//			String[] shippingCategory,
-//			boolean unrelease,
-//			Long orderNo,
-//			String clientName,
-//			Pager pager,
-//			int pageSize
-//			) {
-//		return clientDao.getListByShippingCategory(shippingCategory, unrelease, orderNo, clientName, pager, pageSize);
-//	};
 
 	//미출고 건수 조회
 	public int unreleaseCnt() {
